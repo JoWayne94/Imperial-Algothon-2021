10b8a9151c07f1396ecc8516bb82065c0c2f22cb
Jo Wayne Tan <50750667+JoWayne94@users.noreply.github.com>
2021-04-01 17:37:58 +0100

    Update LowLatency_predict.py

--- Execution/LowLatency_predict.py
+++ Execution/LowLatency_predict.py
@@ -1,3 +1,8 @@
+"""
+Inputs:  A row of 500 windowsized log returns, delimited by commas.
+Outputs: Prediction of 0 and 1, if next timestep log return is going to decrease or increase.
+"""
+
 import numpy as np
 import sys
 from joblib import load
@@ -12,6 +17,6 @@ if __name__ == '__main__':
 
 	for line in sys.stdin:
 
-		X_in = np.array([float(i) for i in line.split(",")])
+		X_in = np.array([float(i) for i in line.split(",")]).reshape(1, -1)
 
-		print(int(knn.predict(X_in.reshape(1, -1)) * 1))
+		print(int(knn.predict(X_in) * 1))
